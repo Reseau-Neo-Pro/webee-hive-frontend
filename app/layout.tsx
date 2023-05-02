e5fb21dc422386610e4b1389c59f17a009341be3
Ulysse <u.hischke@outlook.fr>
2023-05-02 12:04:18 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import { Bebas_Neue } from 'next/font/google'
+
+const font = Bebas_Neue({
+  weight: ['400'],
+  subsets: ['latin']
+})
+
+export const metadata = {
+  title: 'WeBee Hive',
+  description: 'Réseau social pour néo-pro du web',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
