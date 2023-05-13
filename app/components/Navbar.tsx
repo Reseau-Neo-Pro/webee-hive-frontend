bd42c51c26c5ceea8222192641d6cd422b062d5d
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-05-13 13:21:59 +0200

    add logo for testing

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,38 @@
-'use client';
+"use client";
+import { Box, Link, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function Navbar() {
-
   return (
-    <nav>Navbar</nav>
-  )
+    <Stack
+      spacing={4}
+      sx={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        src="/logo-webee-hive.png"
+        alt="logo weebee-hive"
+        width={80}
+        height={80}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          paddingTop: "20px",
+        }}
+      >
+        <Typography sx={{ minWidth: 100 }}>
+          <Link href="/">Contact</Link>
+        </Typography>
+        <Typography sx={{ minWidth: 100 }}>
+          <Link href="/">Contact</Link>
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
