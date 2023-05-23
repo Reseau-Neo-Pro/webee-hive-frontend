035d58f3cf74b7dc1c54a3d6bd5a6d28eca2540e
Laurie <laurie.gazelle@gmail.com>
2023-05-23 16:21:41 +0200

    logo user card, add sass

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,14 +1,31 @@
 'use client';
 import { CiMedal } from 'react-icons/Ci';
+import { AiOutlineRightSquare } from 'react-icons/Ai';
+import cardPicture from '../../../assets/laurie.jpg';
+import codePenLogo from '../../../assets/svg-path.png';
+import pizzaLogo from '../../../assets/Pizza.png';
+import beerLogo from '../../../assets/beer_mug_full.png';
+import htmlLogo from '../../../assets/html5.png';
+import globeLogo from '../../../assets/globe.png';
+import githubLogo from '../../../assets/globe.png';
+import '../card/card.scss';
+
 
 function Card () {
     return (
-        <div className="Card">
+        <div className="card">
             <div className="gratuated">Diplômé depuis <CiMedal/></div>
-            <div className="jobs"></div>
-            <img className="img-card" src=""/>
+            <div className="jobs">Core de métier <AiOutlineRightSquare/></div>
+            <img className="user-picture" alt="user-picture" src={cardPicture.src}/>
             <div className="user-name">Name</div>
-            <span className="user-skills">skills</span>
+            <div className="user-skills">skills
+                <img className="skills" alt="logo-skills" src={codePenLogo.src}/>
+                <img className="skills" alt="logo-skills" src={pizzaLogo.src}/>
+                <img className="skills" alt="logo-skills" src={beerLogo.src}/>
+                <img className="skills" alt="logo-skills" src={htmlLogo.src}/>
+                <img className="skills" alt="logo-skills" src={globeLogo.src}/>
+                <img className="skills" alt="logo-skills" src={githubLogo.src}/>
+            </div>
         </div>
     )
 }
