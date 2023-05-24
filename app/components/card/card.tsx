5ad72004af20e342681c6ee909e6050f51accf30
Laurie <laurie.gazelle@gmail.com>
2023-05-24 15:43:47 +0200

    css

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,30 +1,32 @@
 'use client';
+
 import { CiMedal } from 'react-icons/Ci';
-import { AiOutlineRightSquare } from 'react-icons/Ai';
-import cardPicture from '../../../assets/laurie.jpg';
-import codePenLogo from '../../../assets/svg-path.png';
-import pizzaLogo from '../../../assets/Pizza.png';
-import beerLogo from '../../../assets/beer_mug_full.png';
-import htmlLogo from '../../../assets/html5.png';
-import globeLogo from '../../../assets/globe.png';
-import githubLogo from '../../../assets/globe.png';
+import { AiOutlineRightSquare, AiOutlineCodepen, AiOutlineHtml5, AiOutlineGithub } from 'react-icons/Ai';
+import { CiPizza, CiBeerMugFull, CiGlobe } from 'react-icons/Ci';
+import cardPicture from '../../../assets/profile.png';
+
+
 import '../card/card.scss';
 
 
 function Card () {
     return (
         <div className="card">
-            <div className="gratuated">Diplômé depuis <CiMedal/></div>
-            <div className="jobs">Core de métier <AiOutlineRightSquare/></div>
+            <div className="trade">
+            <div className="graduated"><CiMedal/>1 an</div>
+            <div className="jobs"><AiOutlineRightSquare/></div>
+            </div>
+            <div className="profile">
             <img className="user-picture" alt="user-picture" src={cardPicture.src}/>
             <div className="user-name">Name</div>
-            <div className="user-skills">skills
-                <img className="skills" alt="logo-skills" src={codePenLogo.src}/>
-                <img className="skills" alt="logo-skills" src={pizzaLogo.src}/>
-                <img className="skills" alt="logo-skills" src={beerLogo.src}/>
-                <img className="skills" alt="logo-skills" src={htmlLogo.src}/>
-                <img className="skills" alt="logo-skills" src={globeLogo.src}/>
-                <img className="skills" alt="logo-skills" src={githubLogo.src}/>
+            </div>
+            <div className="user-skills">
+                <div className="skills"><AiOutlineCodepen/></div>
+                <div className="skills"><CiPizza/></div>
+                <div className="skills"><CiBeerMugFull/></div>
+                <div className="skills"><AiOutlineHtml5/></div>
+                <div className="skills"><CiGlobe/></div>
+                <div className="skills"><AiOutlineGithub/></div>
             </div>
         </div>
     )
