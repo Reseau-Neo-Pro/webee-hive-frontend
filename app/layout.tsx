6d3e74663e422ad1e22a27ab815d3d0eace03cd8
Ulysse <u.hischke@outlook.fr>
2023-06-01 14:09:14 +0200

    correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,29 @@
-"use client"
-
-import './globals.css'
-import { Bebas_Neue } from 'next/font/google'
-
-import { darkTheme } from "./theme/themes";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import Navbar from './components/Navbar';
+import "./globals.css";
+import { Bebas_Neue } from "next/font/google";
 
 const font = Bebas_Neue({
-  weight: ['400'],
-  subsets: ['latin']
-})
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'WeBee Hive',
-  description: 'Réseau social pour néo-pro du web',
-}
+  title: "WeBee Hive",
+  description: "Réseau social pour néo-pro du web",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fr">
-      <ThemeProvider theme={darkTheme} >
-        <CssBaseline />
-        <body className={font.className}>
-          <Navbar />
-          {/*Layout Ici */}
-          {/* Navbar */}
-          {children}
-          {/* Footer */}
-        </body>
-      </ThemeProvider>
-    </html >
-  )
+      <body className={font.className}>
+        {/*Layout Ici */}
+        {/* Navbar */}
+        {children}
+        {/* Footer */}
+      </body>
+    </html>
+  );
 }
