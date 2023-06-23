5ad42e3a8574fcf50ee8f910b1fce3da286bf897
Elishaya13 <a.lartillier@gmail.com>
2023-06-23 13:59:06 +0200

    Fix theme searchbar input

--- components/header/Searchbar.tsx
+++ components/header/Searchbar.tsx
@@ -1,14 +1,35 @@
+// import { useTheme } from "@emotion/react";
+
+// const Searchbar = () => {
+//   const theme = useTheme();
+
+//   return (
+//     <div className="border-2 px-3 py-2 rounded-2xl min-w-[300px]">
+//       <input
+//         className={`w-full bg-[${theme.palette.background.main}] focus:outline-none focus:placeholder-transparent`}
+//         type="text"
+//         placeholder="Rechercher sur WeBEE HIVE"
+//       />
+//     </div>
+//   );
+// };
+
+// export default Searchbar;
 import { useTheme } from "@emotion/react";
 
 const Searchbar = () => {
   const theme = useTheme();
 
+  const inputClasses =
+    theme.palette.mode === "light" ? "bg-white" : "bg-transparent";
+
   return (
     <div className="border-2 px-3 py-2 rounded-2xl min-w-[300px]">
       <input
-        className={`w-full bg-[${theme.palette.background.main}] focus:outline-none focus:placeholder-transparent`}
+        className={`w-full ${inputClasses} focus:outline-none focus:placeholder-transparent`}
         type="text"
         placeholder="Rechercher sur WeBEE HIVE"
+        style={{ color: theme.palette.text.main }}
       />
     </div>
   );
