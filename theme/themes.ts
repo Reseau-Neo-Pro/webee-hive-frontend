b59d7ee67f1cc68b99f91b33c1b5a438d981cc73
Ulysse <u.hischke@outlook.fr>
2023-06-23 12:26:28 +0200

    delete use client in theme

--- theme/themes.ts
+++ theme/themes.ts
@@ -1,5 +1,3 @@
- "use client";
-
 export const lightPalette = {
   primary: {
     main: "#000",
@@ -8,28 +6,27 @@ export const lightPalette = {
     main: "#f50057",
   },
   background: {
-    main: "#fff", 
+    main: "#fff",
     grey: "#DFDFE0",
-    yellow : "#F4C600",
+    yellow: "#F4C600",
   },
   text: {
     main: "#000000",
-
   },
 };
 
 export const darkPalette = {
   primary: {
     main: "#fff",
-    greyLight: "#DFDFE0"
+    greyLight: "#DFDFE0",
   },
   secondary: {
     main: "#ff4081",
   },
   background: {
-    main: "#424242", 
+    main: "#424242",
     grey: "#797979",
-    yellow : "#F4C600",
+    yellow: "#F4C600",
   },
   text: {
     main: "#DFDFE0",
