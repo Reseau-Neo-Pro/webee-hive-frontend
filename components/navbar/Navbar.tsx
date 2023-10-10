201d51ac842034ae813190c07876f777c757e389
Emilie Charron <nefalis@gmail.com>
2023-10-10 14:44:39 +0200

    manque miel qui coule

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,13 +6,13 @@ import menuNavBar from "./Menu";
 
 import { FaBars } from "react-icons/fa";
 
+// icone menu
 import { HiOutlineHome, HiOutlineUsers } from "react-icons/hi";
 import { HiOutlineRocketLaunch } from "react-icons/hi2";
 import { AiOutlineMessage } from "react-icons/ai";
 import { MdOutlineEventNote } from "react-icons/md";
 import { IoSettingsOutline } from "react-icons/io5";
 import { BsGlobe } from "react-icons/bs";
-
 
 
 const iconComponents: { [key: string]: React.ReactElement }  = {
