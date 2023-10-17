3a06b088e1f0555aa9a2966d8a37660585407ba9
Nathalie <nat933@free.fr>
2023-10-17 15:39:35 +0200

    refactor : css

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import axios from "axios";
-import { useCallback, FormEvent, useState } from "react";
-import { useRouter } from "next/router";
-import Input from "@/components/input/Input";
+import axios from 'axios';
+import { useCallback, FormEvent, useState } from 'react';
+import { useRouter } from 'next/router';
+import Input from '@/components/input/Input';
 
 // Interface décrivant la structure de l'état initial
 interface Credentials {
@@ -12,8 +12,8 @@ interface Credentials {
 
 // Initialisation de l'état initial du formulaire
 const initialCredentials: Credentials = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 // Composant principal de la page de connexion
@@ -22,80 +22,78 @@ export default function LoginPage() {
   const router = useRouter();
 
   // Fonction qui permet de mettre à jour dynamiquement les valeurs de 'credentials' en fonction des saisies de l'utilisateur dans les champs de formulaire.
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setCredentials((prevCredentials) => ({
-      ...prevCredentials,
-      [e.target.name]: e.target.value,
-    }));
-  }, []);
-
-  // Fonction pour gérer la soumission du formulaire et l'authentification
-  const signIn = useCallback(async (credentials: Credentials) => {
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setCredentials((prevCredentials) => ({
+        ...prevCredentials,
+        [e.target.name]: e.target.value,
+      }));
+    }, 
+[]);
+// Fonction pour gérer la soumission du formulaire et l'authentification
+const signIn = useCallback(
+  async (credentials: Credentials) => {
     try {
       // Envoi une requête HTTP au serveur pour vérifier les identifiants
-      const response = await axios.post("/api/login", credentials);
-
+      const response = await axios.post('/api/login', credentials);
       if (response.status === 200) {
         return { ok: true };
-      } else {
-        console.error("Authentication failed");
+        } else {
+        console.error('Authentication failed');
         return { error: "Wrong Credentials" };
+        }
+      } catch (error) {
+      console.error('Authentication error:', error);
+      return { error: 'Authentication Error' };
       }
-    } catch (error) {
-      console.error("Authentication error:", error);
-      return { error: "Authentication Error" };
-    }
-  }, []);
+    }, 
+[]);
 
-  // Fonction qui est appelée lors de la soumission du formulaire
+// Fonction qui est appelée lors de la soumission du formulaire
   const onSubmit = useCallback(
     async (event: FormEvent) => {
       event.preventDefault();
-
       // Appel de la fonction signIn pour l'authentification
       const callback = await signIn(credentials);
-
       // Gestion de la réponse du backend
       if (callback?.ok) {
-        router.push("/"); // Redirection vers la page d'accueil en cas de succès
+        router.push('/'); // Redirection vers la page d'accueil en cas de succès
       } else {
         console.error(callback?.error);
         // Gére l'erreur d'authentification comme vous le souhaitez en cas d'échec
       }
     }, //chaque fois que credentials, router, ou signIn changent, la fonction onSubmit sera mise à jour avec ces nouvelles valeurs, tout en optimisant les performances en évitant de recréer la fonction inutilement. Cela garantit que votre composant réagit de manière efficace aux changements de ces dépendances.
-    [credentials, router, signIn],
+    [credentials, router, signIn]
   );
 
-  return (
-    // Formulaire de connexion avec des champs email, mot de passe et bouton "Sign in"
-    <form className="text-center" onSubmit={onSubmit}>
-      <div className="flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2">
-        <Input
-          placeholder="Email"
-          name="email"
-          id="email"
-          type="email"
-          onChange={handleChange}
-          value={credentials.email}
-        />
-        <Input
-          placeholder="Password"
-          name="password"
-          id="password"
-          type="password"
-          onChange={handleChange}
-          value={credentials.password}
-        />
-        <button
-          type="submit"
-          className="px-6 py-3 flex items-center justify-center bg-blue-700 border-none text-white hover:bg-blue-500 focus:border focus border-gray-200 transition ease-out"
-        >
-          Sign in
-        </button>
-      </div>
-      <div>
-        You already have an account? <a href="/register">Sign up</a>{" "}
-      </div>
-    </form>
-  );
+return (
+  // Formulaire de connexion avec des champs email, mot de passe et bouton "Sign in"
+  <form className="text-center" onSubmit={onSubmit}>
+    <div className="flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2">
+      <Input
+        placeholder="Email"
+        name="email"
+        id="email"
+        type="email"
+        onChange={handleChange}
+        value={credentials.email}
+      />
+      <Input
+        placeholder="Password"
+        name="password"
+        id="password"
+        type="password"
+        onChange={handleChange}
+        value={credentials.password}
+      />
+      <button
+        type="submit"
+        className="px-6 py-3 flex items-center justify-center bg-blue-700 border-none text-white hover:bg-blue-500 focus:border focus border-gray-200 transition ease-out"
+      >Sign in </button>
+    </div>
+    <div>
+      You already have an account? <a href="/register">Sign up</a>{" "}
+    </div>
+  </form>
+);
 }
